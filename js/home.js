7e1ce51fbb05a30f600eb5ef8c50bdce7d436b1a
Aditta Chakraborty <cseaditta@gmail.com>
2025-08-27 04:14:02 +0600

    cash out feature implemented

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const validPinNumber = 1234;
 
+// add money feature
 document.getElementById("btn-add-money").addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -22,3 +23,43 @@ document.getElementById("btn-add-money").addEventListener("click", function(even
     const totalAvailableBalance = addAmount + availableBalance;
     document.getElementById("available-balance").innerText = totalAvailableBalance;
 });
+
+
+// cash out money feature
+document.getElementById("btn-withdraw").addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    const agentNumber = document.getElementById("agent-number").value;
+    const withdrawAmount = parseInt(document.getElementById("withdraw-amount").value);
+    const pinNumber = parseInt(document.getElementById("add-pin").value);
+    const availableBalance = parseInt(document.getElementById("available-balance").innerText);
+
+    if (agentNumber.length !== 11) {
+        alert("Invalid Account Number");
+        return;
+    }
+
+    if (pinNumber !== validPinNumber) {
+        alert("Invalid Pin Number");
+        return;
+    }
+
+    const totalAvailableBalance = availableBalance - withdrawAmount;
+    document.getElementById("available-balance").innerText = totalAvailableBalance;
+});
+
+
+// toggling feature
+document.getElementById("add-money").addEventListener("click", function() {
+    // document.getElementById("add-money").style.backgroundColor = "bg-[#0874f20d]";
+    // document.getElementById("add-money").style.color = "#0874f2";
+    // document.getElementById("add-money").style.fontWeight = "600";
+    document.getElementById("cash-out-parent").style.display = "none";
+    document.getElementById("add-money-parent").style.display = "block";
+});
+
+document.getElementById("cash-out").addEventListener("click", function() {
+    document.getElementById("add-money-parent").style.display = "none";
+    document.getElementById("cash-out-parent").style.display = "block";
+});
+
